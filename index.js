677f6495054475eb3a6bd2607a4cf294d08fc075
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-02-01 22:51:10 -0800

    Attempting bonus

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ document.body.append(img)
 function newItem(itemSrc, posX, posY){
 let item = document.createElement('img')
 item.src = itemSrc
-item.position = 'fixed'
-item.left = posX
-item.bottom = posY
+item.style.position = 'fixed'
+item.style.left = posX
+item.style.bottom = posY
 document.body.append(item)
 
 item.addEventListener('dblclick', function(){
     item.remove()
 })
 }
+
 
 
 
@@ -39,3 +40,17 @@ newImage('assets/well.png', '500px', '425px');          //Load well
 newItem('assets/sheild.png', '305px', '185px');         //Loading shield
 newItem('assets/sword.png', '500px', '405px');          //Loading sword
 newItem('assets/staff.png', '600px', '100px');          //Loading staff 
+
+
+//BONUS 
+function backgroundImage(){
+let backgroundImage = document.createElement('img')
+backgroundImage.src = 'assets/grass.png'
+backgroundImage.style.position = 'fixed'
+backgroundImage.style.left = '100px'
+backgroundImage.style.bottom = '0'
+document.body.append(backgroundImage)
+}
+
+backgroundImage();
+
